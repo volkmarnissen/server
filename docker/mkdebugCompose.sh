f17987dc454a79adcabce052b3afda93d5b98521
Volkmar Nissen <volkmar.nissen@gmail.com>
2024-07-08 09:25:40 +0200

    Fix "crash" when github access token is invalid Create compose.yaml

--- docker/mkdebugCompose.sh
+++ docker/mkdebugCompose.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sed  '/\- \${PORT_MODBUS2MQTT}\:3000\/tcp/a\ \ \ \ \ \ \- "9229:9229"' <modbus2mqtt-compose.yaml  >modbus2mqtt-dbg-compose.yaml
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sed  '/\- \${PORT_MODBUS2MQTT}\:3000\/tcp/a\ \ \ \ \ \ \- "9229:9229"' <${SCRIPT_DIR}/modbus2mqtt-compose.yaml 
 echo '    command:
       - node
-      - "--inspect-brk=0.0.0.0"
+      - "--inspect=0.0.0.0"
       - "node_modules/@modbus2mqtt/server/dist/modbus2mqtt.js"
       - "--ssl"
       - "/ssl"
       - "--yaml"
-      - "/data" ' >>modbus2mqtt-dbg-compose.yaml
-cat modbus2mqtt-dbg-compose.yaml
+      - "/data" ' 
