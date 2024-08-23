5f0cb199bca1e5e0704304b8932b8a2351586bd3
Volkmar Nissen <volkmar.nissen@gmail.com>
2024-08-23 15:06:47 +0200

    MQTT ran into timeout in Hassio Addon

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ declare global {
     }
   }
 }
-
+const DEFAULT_MQTT_CONNECT_TIMEOUT = 60 * 1000
 export enum MqttValidationResult {
   OK = 0,
   tokenExpired = 1,
@@ -130,7 +130,7 @@ export class Config {
     mqttdiscoveryprefix: 'homeassistant',
     mqttdiscoverylanguage: 'en',
     mqttconnect: {
-      connectTimeout: 60 * 1000,
+      connectTimeout: DEFAULT_MQTT_CONNECT_TIMEOUT,
     },
     httpport: 3000,
     fakeModbus: false,
@@ -250,7 +250,7 @@ export class Config {
 
       Config.config.mqttconnect.connectTimeout = Config.config.mqttconnect.connectTimeout
         ? Config.config.mqttconnect.connectTimeout
-        : 60 * 1000
+        : DEFAULT_MQTT_CONNECT_TIMEOUT
       Config.config.mqttconnect.clientId = Config.config.mqttconnect.clientId ? Config.config.mqttconnect.clientId : 'modbus2mqtt'
       Config.config.mqttconnect.clean = Config.config.mqttconnect.clean ? Config.config.mqttconnect.clean : true
       Config.config.httpport = Config.config.httpport ? Config.config.httpport : 3000
@@ -427,7 +427,7 @@ export class Config {
             delete (config.mqttconnect as any).protocol
             delete (config.mqttconnect as any).addon
             debugAddon('getMqttLoginFromHassio: Read MQTT login data from Hassio')
-
+            config.mqttconnect.connectTimeout = DEFAULT_MQTT_CONNECT_TIMEOUT
             resolve(config.mqttconnect)
           },
           reject
