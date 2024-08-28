0e15c35e8483233026526a178d2493f60184a7bb
modbus2mqtt <info@carcam360.de>
2024-08-28 11:41:06 +0200

    updated release.py

--- docker/release.py
+++ docker/release.py
@@ -18,6 +18,8 @@ class ComponentInfo(NamedTuple):
     releaseTag: str
     numLocalChanges:int
     numRemoteChanges:int
+    hasChanges:bool
+    ignore: bool
 
 class StringReplacement(NamedTuple):
     pattern: str
@@ -42,28 +44,62 @@ def getVersion(basedir, component):
         version =d['version']
         return version
 
+def updateDependencies(basedir, componentInfo, componentInfos):
+        inFile = os.path.join( basedir, componentInfo.name, "package.json")
+        print( inFile)
+        replacements = []
+        for cInfo in componentInfos:
+            name='"@modbus2mqtt/' + cInfo.name + '": "'
+            newValue = name + "^" + cInfo.pkgVersion + '"'
+            if cInfo.name != componentInfo.name :
+                replacements.append( StringReplacement(pattern=name + '.*"' , newValue=newValue) )
+        replaceStringInFile(inFile, inFile,replacements )
+
 def getNpmVersion(component):
     return subprocess.getoutput('npm view --silent @modbus2mqtt/' + component + ' version' )
 
-def getReleaseTag(basedir, component):
+def getReleaseTag(basedir, component) ->int:
     pwd = os.getcwd()
     os.chdir(os.path.join(basedir, component))
     nc=subprocess.getoutput('git tag --sort=version:refname | tail -1')
     os.chdir(pwd)
     return nc
 
+def ignore(component,ignoreList):
+    search = ', *' + component + ' *,'  
+    ignore = ',' + ignoreList+ ','
+    if  re.search(search , ignore):
+       print("Ignoring " + component )
+       return True
+    else:
+        return False
+
+
+def hasChanges(basedir, component):
+    pwd = os.getcwd()
+    tag = getReleaseTag(basedir, component)
+    cwd = os.path.join(basedir, component)
+    nc = int(subprocess.getoutput("cd " + cwd + ">/dev/null && git diff tags/" + tag + " HEAD| wc -l" ))
+    os.chdir(pwd)
+    if  nc > 0 :
+       return True 
+    else:
+       return False
+
 def numberOfRemoteChanges(basedir,component):
     pwd = os.getcwd()
     os.chdir(os.path.join(basedir, component))
     nc=subprocess.getoutput('git diff --name-only heads/main origin/main | wc -l')
+
     os.chdir(pwd)
     return nc
 
 
-def numberOfChanges(basedir,component):
+def numberOfChanges(basedir,component)->int:
     pwd = os.getcwd()
     os.chdir(os.path.join(basedir, component))
-    nc=subprocess.getoutput('git status --porcelain| wc -l')
+    nc = int(subprocess.getoutput('git status --porcelain| wc -l'))
+    print("numberOfChanges", component, nc)
     os.chdir(pwd)
     return nc
 
@@ -89,8 +125,33 @@ def releaseComponent(basedir, componentInfo):
         print('push', out, err)
     return return_code
 
-def npmInstall(basedir, componentInfo):
-    print("npmInstall", basedir, componentInfo)
+def npmInstall(basedir, componentName):
+    print("npm install", basedir,  componentName)
+    result = subprocess.Popen(['npm', 'install', '--package-lock-only'],
+		    cwd=os.path.join(basedir, componentName),
+		    stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE) 
+    out, err = result.communicate()
+
+def gitCommit(basedir, componentInfo):
+    print("git commit", basedir,  componentInfo.name)
+    result = subprocess.Popen(['git add . && git commit -m "Update package.json for ' + componentInfo.pkgVersion + '"'],
+		    cwd=os.path.join(basedir, componentInfo.name),
+            shell=True,
+		    stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE) 
+    out, err = result.communicate()
+    print( out.decode("utf-8").rstrip() ,err.decode("utf-8").rstrip()) 
+
+def git(basedir, componentName, command):
+    print("git " + command, basedir,  componentName)
+    result = subprocess.Popen(['git ' + command ],
+		    cwd=os.path.join(basedir, componentName),
+            shell=True,
+		    stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE) 
+    out, err = result.communicate()
+    print( out.decode("utf-8").rstrip() ,err.decode("utf-8").rstrip()) 
 
 def npmPack(basedir, addonDir, name):
     print("npm pack", basedir)
@@ -114,13 +175,22 @@ def npmBuildServer(basedir):
     print(out, err)
     return result.returncode
 
+
 def replaceStringInFile(inFile, outFile, replacements):
+    out=[]
+    for repl in replacements:
+        print( "replacements: " , repl.pattern, repl.newValue)
     with open(inFile, 'r') as r:
-        with open(outFile, 'w') as w:
             for line in r:
                 for repl in replacements:
-                    line = re.sub(rf"{repl.pattern}", repl.newValue,line)
-                w.write( line)
+                    lineNew = re.sub(rf"{repl.pattern}", repl.newValue,line)
+                    if lineNew != line:
+                        print( "Replace with ", lineNew)
+                    line = lineNew
+                out.append(lineNew)
+    with open(outFile, 'w') as w:
+        for line in out:
+            w.write( line)
 
 def prepareConfigYaml( basedir, serverVersion):
     inFile = os.path.join(basedir, server,'hassio-addon', configYaml)
@@ -205,26 +275,49 @@ def reset(tarinfo):
     tarinfo.uname = tarinfo.gname = "root"
     return tarinfo
 
+def buildComponentInfo( basedir, componentName, ignoreList):
+    return ComponentInfo(componentName, 
+                        getVersion(basedir, componentName),
+                        getNpmVersion(componentName),
+                        getReleaseTag(basedir, componentName),
+                        numberOfChanges(basedir, componentName),
+                        numberOfRemoteChanges(basedir, componentName),
+			            hasChanges(basedir, componentName),
+                        ignore(componentName, ignoreList))
+
+
+def test():
+    name='"@modbus2mqtt/angular'  + '": "'
+    newValue = name + "^" + "0.12.2" + '"'
+    replacements = []
+    replacements.append( StringReplacement(pattern=name + '.*"' , newValue=newValue) )
+    name='"@modbus2mqtt/specification.shared'  + '": "'
+    newValue = name + "^" + "0.17.2" + '"'
+    replacements.append( StringReplacement(pattern=name + '.*"' , newValue=newValue) )
+    replaceStringInFile("package.json", "package.json.new",replacements )
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--basedir", help="base directory of all repositories", default='.')
 parser.add_argument("-d", "--debug", help="Creates local addon directory for debugging", action='store_true')
 parser.add_argument("-p", "--sshport", help="Sets the ssh port for addon directory default: 22",  nargs='?', default=22, type=int)
 parser.add_argument("-s", "--sshhost", help="Sets the ssh host for addon directory default: localhost", nargs='?', default='localhost', const='localhost')
+parser.add_argument("-i", "--ignorelist", help="comma separated list of components which should not be released", nargs='?', default='' )
 
 args = parser.parse_args()
 componentInfos=[]
 serverComponent=""
 for component in components:
-    ci = ComponentInfo(component, getVersion(args.basedir, component),
-                        getNpmVersion(component),
-                        getReleaseTag(args.basedir, component),
-                        numberOfChanges(args.basedir, component),
-                         numberOfRemoteChanges(args.basedir, component))
-    componentInfos.append( ci )
+    print(component)
+    git(args.basedir, component, "pull")        
+    npmInstall(args.basedir, component)
+    ci = buildComponentInfo( args.basedir, component, args.ignorelist)
+    
+    componentInfos.append( buildComponentInfo( args.basedir, component, args.ignorelist) )
+    print(ci)
     if component == server:
         serverComponent = ci
 
-    print(componentInfos)
 
 if( args.debug):
     print("debug")
@@ -238,22 +331,36 @@ if( args.debug):
     cleanAddonDir(addonDir,tgzs)
     executeCpSh(args.sshport, args.sshhost)
 else:
-    for componentInfo in componentInfos:        
-        if int(componentInfo.numLocalChanges) > 0:
-            print( componentInfo.name +  ' has local changes, please commit first' )
+    for componentInfo in componentInfos:
+        print( "componentInfo:" + componentInfo.name)
+        updateDependencies(args.basedir, componentInfo, componentInfos) 
+        numOfChanges = int(numberOfChanges(args.basedir, componentInfo.name))
+        if not componentInfo.ignore and int(numOfChanges) > 0:
+            print( componentInfo.name + " has local changes ", componentInfo.numLocalChanges)
+            if componentInfo.numLocalChanges == 0 :
+                print( componentInfo.name + " has only package.json changes")
+                npmInstall(args.basedir, component)
+                gitCommit(args.basedir, componentInfo)
+                git(args.basedir, component, "push")
+                numOfChanges = int(numberOfChanges(args.basedir, componentInfo.name))
+                if int(numOfChanges) > 0:
+                    print( componentInfo.name +  ' has ' + str( numOfChanges ) + ' local changes, please commit first' )
+                    exit(2)
+                componentInfo = buildComponentInfo(args.basedir, componentInfo.name, args.ignorelist)
+
+        if componentInfo.ignore :
+            print( componentInfo.name +  " will be ignored")
         else:
             if int (componentInfo.numRemoteChanges) > 0:
                 print( componentInfo.name +  ' has differences to remote push/pull first' )
             else:
-                if componentInfo.pkgVersion == componentInfo.npmVersion:
-                    print( componentInfo.name +  ' is up to date' )
-                else:
-                    if 'v'+ componentInfo.pkgVersion == componentInfo.releaseTag:
-                        print(component + '/' + componentInfo.pkgVersion +  ' is outdated in npm but has up to date release tag ' + componentInfo.releaseTag)
+                if componentInfo.hasChanges:
+                    if  componentInfo.pkgVersion == componentInfo.npmVersion  :
+                        print( componentInfo.name + " please update package.json version" )
+                        exit(3)
+                    if releaseComponent(args.basedir, componentInfo) == 0:
+                        print(componentInfo.name + " released successfully")
                     else:
-                        print( "component will be released ")
-                        print( componentInfo )
-                        if releaseComponent(args.basedir, componentInfo) == 0:
-                            print(componentInfo.name + " released successfully")
-                        else:
-                            print(componentInfo.name + " released failed")
+                        print(componentInfo.name + " released failed")
+                else:
+                    print("no changes since last release")
