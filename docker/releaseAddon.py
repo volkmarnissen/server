e408d44a9093e3535f85d47a211af4e4f39b7702
volkmarnissen <volkmar.nissen@gmail.com>
2025-02-28 11:33:48 +0100

    fixed releaseAddon.py

--- docker/releaseAddon.py
+++ docker/releaseAddon.py
@@ -5,6 +5,8 @@ import re
 import json
 import sys
 import tarfile
+import subprocess
+from typing import NamedTuple
  
 server ='server'
 hassioAddonRepository= 'hassio-addon-repository'
@@ -12,8 +14,12 @@ hassioAddonRepository= 'hassio-addon-repository'
 modbus2mqtt ='modbus2mqtt'
 configYaml='config.yaml'
 dockerDir ='docker'
-DockerfileTemplate = 'Dockerfile.template'
 dockerFile = 'Dockerfile'
+
+class StringReplacement(NamedTuple):
+    pattern: str
+    newValue:str
+
 
 def getVersion(basedir, component):
     with open(os.path.join(basedir, component,'package.json'), 'r') as f:
@@ -21,67 +27,76 @@ def getVersion(basedir, component):
         version =d['version']
         return version
 
-def replaceStringInFile(inFile, outFile, replaceName, replaceValue):
+def getLatestClosedPullRequest():
+    d = json.loads(subprocess.getoutput('gh pr list -s closed -L 1 --json number' ))
+    return d[0]['number']
+
+def getVersionForDevelopment(basedir, component):
+    prnumber = getLatestClosedPullRequest()
+    version = getVersion(basedir, component)
+    return version + "-pr" + str(prnumber)
+
+
+
+def replaceStringInFile(inFile, outFile, replacements):
+    out=[]
+    for repl in replacements:
+        print( "replacements: " , repl.pattern, repl.newValue)
+    with open(inFile, 'r') as r:
+            for line in r:
+                for repl in replacements:
+                    lineNew = re.sub(rf"{repl.pattern}", repl.newValue,line)
+                    if lineNew != line:
+                        print( "Replace with ", lineNew)
+                    line = lineNew
+                if( lineNew != "" ):
+                    out.append(lineNew)
+    with open(outFile, 'w') as w:
+        for line in out:
+            w.write( line)
+
+def replaceAndDeleteStringInFile(inFile, outFile, replaceName, replaceValue, deleteName):
     with open(inFile, 'r') as r:
         with open(outFile, 'w') as w:
             for line in r:
-                line = re.sub(rf"{replaceName}", replaceValue,line)
-                w.write( line)
-
-
-def reset(tarinfo):
-    tarinfo.uid = tarinfo.gid = 0
-    tarinfo.uname = tarinfo.gname = "root"
-    return tarinfo
-
+                if( not line.startswith(deleteName) ):
+                    line = re.sub(rf"{replaceName}", replaceValue,line)
+                    w.write( line)
 
 # runs in (@modbus2mqtt)/server
 # updates config.yaml in (@modbus2mqtt)/hassio-addon-repository
-def createAddonDirectoryForRelease(basedir,version):
+def updateConfigYamlVersion(basedir,version, replacements):
     sys.stderr.write("createAddonDirectory release " + basedir  + " " +  version + "\n")
-    replaceStringInFile(os.path.join(basedir, server, 'hassio-addon', configYaml), \
-        os.path.join(basedir, hassioAddonRepository,modbus2mqtt,  configYaml),"<version>", version )
-
-def createAddonDirectoryForDebug(basedir):
-    sys.stderr.write("createAddonDirectory debug " +  basedir + "\n") 
-
-# runs in (@modbus2mqtt)/server
-# Creates rootfs.tar in (@modbus2mqtt)/hassio-addon-repository
-def createDockerDirectoryForRelease(basedir, version):
-    sys.stderr.write("createDockerforRelease " + basedir + " "  + version  + "\n")
-    
-
-
-def createDockerDirectoryForDebug(basedir):
-    sys.stderr.write("createDockerforDebug  " +  basedir)
-
+    serverP = os.path.join(basedir, server, 'hassio-addon', configYaml)
+    hassioP = os.path.join(basedir, hassioAddonRepository,modbus2mqtt,  configYaml)
+    replaceStringInFile(serverP,hassioP, replacements)
 
 # publishes docker image from (@modbus2mqtt)/hassio-addon-repository
 # docker login needs to be executed in advance 
 def pusblishDocker(basedir, version):
     sys.stderr.write("publishDocker "  + basedir + " " + version)
 
-# copy addon directory from (@modbus2mqtt)/hassio-addon-repository
-# docker login needs to be executed in advance 
-def copyAddon(basedir, host,port ):
-    sys.stderr.write("copy addon " + basedir + " " +  host )
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--basedir", help="base directory of all repositories", default='.')
-parser.add_argument("-r", "--release", help="releases Dockerfile for production", action='store_true')
-parser.add_argument("-d", "--debug", help="Creates local addon directory for debugging", type=bool)
-parser.add_argument("-p", "--sshport", help="Sets the ssh port for addon directory default: 22",  nargs='?', default=None, const=22, type=int)
-parser.add_argument("-s", "--sshhost", help="Sets the ssh host for addon directory default: localhost", nargs='?', default='localhost', const='localhost')
+parser.add_argument("-R", "--ref", help="ref branch or tag ", default='refs/heads/main')
+parser.add_argument("-r", "--release", help="builds sets version number in config.yaml", action='store_true')
 
 args = parser.parse_args()
-
-if args.release:
+if args.release or args.ref.endswith("release"):
     version = getVersion(args.basedir, 'server')
-    createDockerDirectoryForRelease(args.basedir, version)
-    createAddonDirectoryForRelease(args.basedir, version)
-    # pusblishDocker(args.basedir, version)
+    replacements = [
+        StringReplacement(pattern='<version>', newValue=version),
+        ]
+    updateConfigYamlVersion(args.basedir, version, replacements)
     print("TAG_NAME=v" + version)
 else:
-    createDockerDirectoryForDebug(args.basedir)
-    createAddonDirectoryForDebug(args.basedir)
-    copyAddon(args.basedir,args.sshhost, args.sshport)
+    version = getVersionForDevelopment(args.basedir, 'server' )
+    replacements = [
+        StringReplacement(pattern='<version>', newValue=version),
+        StringReplacement(pattern='image:.*', newValue=''),
+        StringReplacement(pattern='slug:.*', newValue='slug: modbusdev'),
+        StringReplacement(pattern='codenotary:.*', newValue=''),
+        ]
+    updateConfigYamlVersion(args.basedir, version, replacements)
+    print("TAG_NAME=v" + version)
+
