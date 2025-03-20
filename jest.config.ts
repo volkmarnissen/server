f669a3e0b0f2eba559c02b759bb21cc68a8724c5
volkmarnissen <volkmar.nissen@gmail.com>
2025-03-20 10:52:24 +0100

    jest: coverage and junit xml generation

--- jest.config.ts
+++ jest.config.ts
@@ -11,5 +11,17 @@ const config: Config = {
   testRegex: '/__tests__/(.*|(\\.|/)(test|spec))(\\.tsx)+$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   //  setupFiles: ["<rootDir>/.jest/setEnvVars.js"]
+  collectCoverage: true,
+  coverageDirectory:'./',
+  coveragePathIgnorePatterns: ['/node_modules/', '__test__/'],
+  coverageReporters: [ "json-summary"],
+  "reporters": ["default", ["jest-junit", { 
+    "suiteName": "jest tests",
+    "outputDirectory": ".",
+    "outputName": "junit.xml",
+    "uniqueOutputName": "false",
+    "classNameTemplate": "{filename}",
+    "titleTemplate": "{title}",
+    "suiteNameTemplate": "{filename}"} as any]]
 }
 export default config
