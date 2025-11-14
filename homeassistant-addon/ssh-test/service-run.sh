ab0efdfab28d7bee6b13f62e0c79b6ffb24eff86
Volkmar Nissen <info@carcam360.de>
2025-11-14 12:20:04 +0100

    removed homeassistant-addon

--- homeassistant-addon/ssh-test/service-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Service run script (s6 services.d): keep minimal, no privileged setup here
-# sshd will run in the foreground
-
-exec /usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
