13b453e2c2738ab91becfebe38c4ecb340bd2034
volkmarnissen <volkmar.nissen@gmail.com>
2025-11-14 16:47:48 +0100

    Add comment in configure-modbus2mqtt.sh

--- docker/rootfs/etc/s6-overlay/scripts/configure-modbus2mqtt.sh
+++ docker/rootfs/etc/s6-overlay/scripts/configure-modbus2mqtt.sh
@@ -1,12 +1,17 @@
-    #!/command/with-contenv sh
-    [ ! -d "/config/modbus2mqtt" ] && { 
-        mkdir -p /config/modbus2mqtt; 
-        [ -d /data/local ] && cp -R /data/local/* /config/modbus2mqtt/; 
-    }
-    [ ! -d "/data/public" ] && mkdir -p /data/public; 
-    chown -R modbus2mqtt:dialout /config/modbus2mqtt
-    chown -R modbus2mqtt:dialout /data/public
-    touch /ssl/secrets.txt
-    chown -R modbus2mqtt:dialout /ssl/secrets.txt
+#!/command/with-contenv sh
+# the shared volumes may not be mounted when this script runs
+# if they are mounted, ensure required directories have correct ownership as 
+# this script runs as root
+# Migration from old config location /data/local to /config/modbus2mqtt
+# If /config/modbus2mqtt does not exist, copy configuration from /data/local
+[ ! -d "/config/modbus2mqtt" ] && { 
+    mkdir -p /config/modbus2mqtt; 
+    [ -d /data/local ] && cp -R /data/local/* /config/modbus2mqtt/; 
+}
+[ ! -d "/data/public" ] && mkdir -p /data/public; 
+chown -R modbus2mqtt:dialout /config/modbus2mqtt
+chown -R modbus2mqtt:dialout /data/public
+touch /ssl/secrets.txt
+chown -R modbus2mqtt:dialout /ssl/secrets.txt
 
  
